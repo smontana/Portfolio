05eb404655c9ff5e5c8cceadfa80c7280827855b
Stephen Montana <stephen.montana@sunnova.com>
2024-05-30 16:09:58 -0500

    add tailwind screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+			phablet: '640px',
+			tabPortrait: '768px',
+			tabLandscape: '1024px',
+			laptop: '1280px',
+			desktop: '1440px',
+			wide: '1800px',
+		},
     extend: {},
   },
   plugins: [],
