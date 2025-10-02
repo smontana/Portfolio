874500100d8dbee5a69b70f4da67795ad1bba147
Stephen Montana <stephen.montana@sonnysdirect.com>
2025-10-02 16:06:38 -0500

    fixed Navbar positioning issue by moving up a level along with Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components';
-import { About, Home, Work } from "./pages";
-
 function App() {
   return (
     <>
-      <Router>
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/work" element={<Work />} />
-          {/* <Route path="/contact" element={<Contact />} /> */}
-        </Routes>
-      </Router>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Navbar } from './components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { About, Home, Work } from "./pages";
 import "./styles/index.scss";
 import 'react-tooltip/dist/react-tooltip.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="container">
-      <App />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+      <div className="container">
+        <App />
+      </div>
+    </Router>
   </React.StrictMode>
 );
