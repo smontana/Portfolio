810acd483b2b2d518179f474bcbfe6a7235fdc3e
Stephen Montana <stephen.montana@sonnysdirect.com>
2025-10-02 17:18:40 -0500

    updated socials icons and css

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Style.scss";
 import { AiFillLinkedin, AiOutlineGithub } from "react-icons/ai";
 
@@ -6,10 +5,10 @@ export const Socials = () => {
   return (
     <div className="icons">
       <a href="https://github.com/smontana" target="_blank" rel="noreferrer">
-        <AiOutlineGithub style={{ color: "#4078c0" }} />
+        <AiOutlineGithub style={{ color: "#B6FF30" }} />
       </a>
       <a href="https://www.linkedin.com/in/stephen-montana/" target="_blank" rel="noreferrer">
-        <AiFillLinkedin style={{ color: "#0072b1 " }} />
+        <AiFillLinkedin style={{ color: "#B6FF30" }} />
       </a>
     </div>
   );
