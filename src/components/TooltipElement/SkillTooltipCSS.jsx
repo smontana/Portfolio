4cbe3c1c860372f26fdb57a488cf7eb91387aa9a
Stephen Montana <stephen.montana@sonnysdirect.com>
2025-10-16 12:38:26 -0500

    fixed issue with tooltip CSS

--- src/components/TooltipElement/SkillTooltipCSS.jsx
+++ src/components/TooltipElement/SkillTooltipCSS.jsx
@@ -1,8 +1,16 @@
+import "./Style.scss";
+
 // Lightweight CSS-only tooltip component
 export function SkillTooltip({ Icon, color, label }) {
   return (
-    <div className="skill-tooltip-wrapper" data-tooltip={label}>
-      <Icon style={{ color }} aria-label={label} />
+    <div
+      className="skill-tooltip-wrapper"
+      data-tooltip={label}
+      tabIndex={0}
+      role="img"
+      aria-label={label}
+    >
+      <Icon style={{ color }} />
     </div>
   );
 }
