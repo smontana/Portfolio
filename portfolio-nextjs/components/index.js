9172f821b65b82f39155cafe7573c94a35702f6e
Stephen Montana <stephen.montana@sonnysdirect.com>
2025-10-31 16:48:02 -0500

    removed unused Loader component to reduce bundle size

--- portfolio-nextjs/components/index.js
+++ portfolio-nextjs/components/index.js
@@ -1,7 +1,6 @@
 export * from './Card/Card';
 export * from './Form/Form';
 export * from './Heading/Heading';
-export * from './Loader/Loader';
 export * from './Navbar/Navbar';
 export * from './Socials/Socials';
 export * from './StephenSVG/StephenSVG';
