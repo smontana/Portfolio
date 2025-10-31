96b881ea771a83a0d77ff2664ebf1fafbbef18a3
Stephen Montana <stephen.montana@sonnysdirect.com>
2025-10-31 15:30:34 -0500

    replace apostrophes with unicode

--- portfolio-nextjs/app/about/page.js
+++ portfolio-nextjs/app/about/page.js
@@ -31,8 +31,8 @@ export default function AboutPage() {
             As a dedicated, Full Stack Software Developer, I specialize in creating applications that solve problems and delight users.
             With over 10 years of development experience, I thrive at solving problems through the creation of thoughtful solutions.
             My journey has been marked by a commitment to fostering a culture of innovation, collaboration, and continuous learning.
-            Whether it's shaping scalable architectures, implementing cutting-edge technologies, or optimizing performance for exceptional user experiences,
-            I'm driven by a relentless pursuit of excellence.
+            Whether it&apos;s shaping scalable architectures, implementing cutting-edge technologies, or optimizing performance for exceptional user experiences,
+            I&apos;m driven by a relentless pursuit of excellence.
             I work to bridge the gap between business objectives and technical execution, translating visions into tangible outcomes that exceed expectations.
           </p>
         </div>
